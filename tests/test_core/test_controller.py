07dcb20bfe436119b2d56c39a66706930e11a73c
Nass <nassim.benchabane.1@gmail.com>
2023-10-19 12:38:54 -0400

    minor edit. Gonna attempt shared memory for controller

--- tests/test_core/test_controller.py
+++ tests/test_core/test_controller.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
 
 """Idea: Create a multiprocess. One will receive input through the builtin input() function, the other one sends the desired input. Then, assess that the received input is the same."""
-
+"""
+Another idea: into the actual inputs/controller classes, for the exported DLL functions, add an .errcheck attribute with a callable. The callable is used to validate the return type of the function.
+This could be handy for testing purposes.
+"""
 
 class TestController(TestCase):
     def test_activate(self):
