2cf0a11d8cb135003e1d812c3696412f91c6009c
Nass <nassim.benchabane.1@gmail.com>
2024-01-09 20:56:00 -0500

    Refactored path of time

--- royals/maps/__init__.py
+++ royals/maps/__init__.py
@@ -4,3 +4,4 @@ Maps are merely containers in which to find Mobs and a Minimap, along with conne
 from .buddha_encounter import EncounterWithTheBuddha
 from .line_1_area_1 import Line1Area1
 from .mysterious_path_3 import MysteriousPath3
+from .path_of_time_1 import PathOfTime1
