010b0e813a3390e19ead06825103f433c2d2c2d3
Nass <nassim.benchabane.1@gmail.com>
2024-01-29 21:37:32 -0500

    not much

--- toolkit/class_tester.py
+++ toolkit/class_tester.py
@@ -17,23 +17,23 @@ HANDLE = client_handler.get_client_handle("WrongDoor", royals_ign_finder)
 import win32api
 
 if __name__ == "__main__":
-
-    inv = InventoryMenu()
-    minimap = PathOfTime1Minimap()
-    while True:
-        inv.is_extended(HANDLE)
-        # print(inv.read_item_name(HANDLE, controller.get_mouse_pos(HANDLE)))
-        # img = take_screenshot(HANDLE)
-        # cv2.imshow('client_img', img)
-        # # extend_button = inv.get_abs_box(HANDLE, inv.extend_button)
-        # boxes = inv.get_all_slots_boxes(HANDLE, img)
-        #
-        # for box in boxes:
-        #     target = box.random()
-        #     asyncio.run(controller.mouse_move(HANDLE, target, total_duration=0.1))
-
-
-        # asyncio.run(controller.mouse_move(HANDLE, extend_button.random()))
-        # breakpoint()
-        #
-        # print('Space left:', inv.get_space_left(HANDLE, img))
+    asyncio.run(controller.move(HANDLE, "WrongDoor", "right", 2, secondary_direction="up"))
+#     inv = InventoryMenu()
+#     # minimap = PathOfTime1Minimap()
+#     # while True:
+#         # inv.is_extended(HANDLE)
+#         # print(inv.read_item_name(HANDLE, controller.get_mouse_pos(HANDLE)))
+#         # img = take_screenshot(HANDLE)
+#         # cv2.imshow('client_img', img)
+#         # # extend_button = inv.get_abs_box(HANDLE, inv.extend_button)
+#         # boxes = inv.get_all_slots_boxes(HANDLE, img)
+#         #
+#         # for box in boxes:
+#         #     target = box.random()
+#         #     asyncio.run(controller.mouse_move(HANDLE, target, total_duration=0.1))
+#
+#
+#         # asyncio.run(controller.mouse_move(HANDLE, extend_button.random()))
+#         # breakpoint()
+#         #
+#         # print('Space left:', inv.get_space_left(HANDLE, img))
