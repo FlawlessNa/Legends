836c7bfecf83f7e9894d504cee21dd93954560e3
Nassim Benchabane <Nassim.Benchabane@oliverwyman.com>
2024-07-29 10:50:07 -0400

    Implemented basic parser for discord using argparse module

--- botting/communications/base_parser.py
+++ botting/communications/base_parser.py
@@ -1,12 +1,16 @@
+import argparse
 import logging
 from abc import ABC, abstractmethod
 
 logger = logging.getLogger(__name__)
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.NOTSET
 
 
 class BaseParser(ABC):
     """
+    # TODO - Implement Discord confirmation and/or error message from user-input.
+     (EG: if input is wrong, send a discord message to inform user. If input is correct,
+     send confirmation to user).
     Implements basic to interpret discord messages received by the user.
     User messages should have the general format:
     <ACTION> --ign <IGN> (optional) --args <ARGS> (varies based on ACTION)
@@ -17,6 +21,35 @@ class BaseParser(ABC):
     RESUME --ign IgnFromAPausedBot
     WRITE --ign IgnFromABot --m Message to write
     """
+    action_choices = ['KILL', 'PAUSE', 'RESUME', 'WRITE']
+    action_choices.extend(action.lower() for action in action_choices)
+    action_choices.extend(action.title() for action in action_choices)
+
+    def __init__(self) -> None:
+        self.parser = argparse.ArgumentParser(
+            description='Parse messages from discord user.',
+            exit_on_error=False
+        )
+        self.parser.add_argument(
+            'action',
+            type=str,
+            help='The action to perform.',
+            choices=self.action_choices,
+        )
+        self.parser.add_argument(
+            '--ign',
+            type=str,
+            help='The IGN of the bot to perform the action on.',
+            required=False,
+            nargs='+'
+        )
+        self.parser.add_argument(
+            '-m', '--message',
+            type=str,
+            help='The message to write.',
+            nargs='+',
+            required=False,
+        )
 
     def parse_message(self, message: str):
         """
@@ -24,29 +57,41 @@ class BaseParser(ABC):
         :param message: The message to parse.
         :return: TBD.
         """
-        lst = message.split(" ")
-        action = lst[0]
-        if action.upper() == 'KILL':
+        args = self.parser.parse_args(message.split())
+        action = args.action.upper()
+
+        if action == 'KILL':
+            if args.ign or args.message:
+                logger.warning(
+                    "KILL action does not accept --ign or --message arguments. "
+                    "They will be ignored."
+                )
             return self.kill()
-        elif action.upper() == 'PAUSE':
-            if '--ign' in lst:
-                who = lst[lst.index('--ign') + 1:]
-            else:
-                who = None
-            return self.pause(who)
-        elif action.upper() == 'RESUME':
-            if '--ign' in lst:
-                who = lst[lst.index('--ign') + 1:]
-            else:
-                who = None
-            return self.resume(who)
-        elif action.upper() == 'WRITE':
-            if '--ign' in lst:
-                who = lst[lst.index('--ign') + 1]
-            else:
-                who = None
-            message = ' '.join(lst[lst.index('--m') + 1:])
-            return self.write(message, who)
+
+        elif action == 'PAUSE':
+            if args.message:
+                logger.warning(
+                    "PAUSE action does not accept --message argument. "
+                    "It will be ignored."
+                )
+            return self.pause(args.ign)
+
+        elif action == 'RESUME':
+            if args.message:
+                logger.warning(
+                    "RESUME action does not accept --message argument. "
+                    "It will be ignored."
+                )
+            return self.resume(args.ign)
+
+        elif action == 'WRITE':
+            if not args.message:
+                logger.error(
+                    "WRITE action requires a --message argument. "
+                    "Please provide one."
+                )
+                return
+            return self.write(' '.join(args.message), args.ign)
         else:
             raise ValueError(f"Invalid action: {action}")
 
