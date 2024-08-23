cbaab2b3a2adc94249e3e0c5b28525304f922802
Nass <nassim.benchabane.1@gmail.com>
2024-08-23 11:35:41 -0400

    Added training bot

--- royals/bots/__init__.py
+++ royals/bots/__init__.py
@@ -1,2 +1,3 @@
 from .test_bot import TestBot
 from .leeching import LeechingBot
+from .training import TrainingBot
---
+++ royals/launchers/launcher_training_v2.py
@@ -0,0 +1,32 @@
+import asyncio
+from botting.core.session_manager import SessionManager
+import royals.bots as bots
+import royals.model.maps as maps
+import royals.parsers
+
+training_configs = {
+    "num_pets": 1,
+    "game_map": maps.Line1Area1,
+    "mob_count_threshold": 1,
+    "detection_configs": "Elephant Cape",
+    "client_size": "large",
+    "anti_detection_mob_threshold": 4,
+    "anti_detection_time_threshold": 10,
+    "included_buffs": ["Magic Guard"],
+}
+
+
+async def main():
+    with SessionManager(royals.parsers.RoyalsParser) as session:
+        leecher = bots.TrainingBot("Makeover", session.metadata, **training_configs)
+        # mule1 = bots.TestBot('UluLoot', session.metadata)
+        # mule2 = bots.TestBot('FinancialWiz', session.metadata)
+        # mule3 = bots.TestBot('MoneyEngine', session.metadata)
+        # mule4 = bots.TestBot('iYieldMoney', session.metadata)
+        # mule5 = bots.TestBot('BCoinFarm', session.metadata)
+        # await session.launch([leecher], [mule1, mule2, mule3, mule4, mule5])
+        await session.launch([leecher])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
