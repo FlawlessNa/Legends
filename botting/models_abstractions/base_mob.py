Model usage improved. Still need to handle debug mode and fix rotation + mobs hitting makers

botting/models_abstractions/base_mob.py
@@ -46,19 +46,25 @@ class BaseMob(InGameDetectionVisuals, ABC):
         handle: int,  # must be provided if detection model is used
         threshold: float = None,
         debug: bool = True
-    ) -> list[Sequence[int]]:
+    ) -> tuple[Sequence[int]]:
         """
         Returns a list of tuples of the coordinates for each mob found on-screen.
         If a detection model exists for the current mob, it is used by default.
         :return: Coordinates are, in order, x, y, width, height.
         """
         if self.detection_model is not None:
-            res = self.run_detection_model(
+            detections = self.run_detection_model(
                 handle,
                 image,
                 threshold
             )
-            breakpoint()
+            res = tuple(
+                [
+                    dct["box"].values() for dct in detections.summary()
+                    if dct["name"] == self.__class__.__name__
+                ]
+            )
+            return tuple(map(lambda vals: tuple(map(round, vals)), res))  # noqa
         else:
             processed = self._preprocess_img(image)
             contours, _ = cv2.findContours(
@@ -71,12 +77,12 @@ class BaseMob(InGameDetectionVisuals, ABC):
             except Exception as e:
                 breakpoint()
 
-    def get_mob_count(self, image: np.ndarray, **kwargs) -> int:
+    def get_mob_count(self, handle: int, image: np.ndarray, **kwargs) -> int:
         """
         Returns the number of mobs found on-screen.
         """
         return round(
-            len(self.get_onscreen_mobs(image, **kwargs)) / self._multiplier
+            len(self.get_onscreen_mobs(image, handle, **kwargs)) / self._multiplier
         )
 
 

botting/visuals/in_game_visuals.py
@@ -312,7 +312,7 @@ class InGameDetectionVisuals(InGameBaseVisuals, ABC):
         else:
             for parent in cls.__bases__:
                 if issubclass(parent, InGameDetectionVisuals):
-                    return parent._is_detectable_by_model(detectable_classes)
+                    return parent._is_detectable_by_model(model)
         return False
 
     @classmethod
@@ -320,7 +320,8 @@ class InGameDetectionVisuals(InGameBaseVisuals, ABC):
         cls,
         cache_id: int,
         image: np.ndarray,
-        threshold: float = DEFAULT_THRESHOLD
+        threshold: float = DEFAULT_THRESHOLD,
+        debug: bool = False,
     ) -> Results:
         """
         Run the detection model on the image, and cache the result.
@@ -343,4 +344,10 @@ class InGameDetectionVisuals(InGameBaseVisuals, ABC):
         else:
             print(f"{cls.__name__} using cached predictions.")
 
-        return cls._prediction_cache[cache_id]
+        if debug:
+            cv2.imshow(
+                'Detection Model',
+                InGameDetectionVisuals._prediction_cache[cache_id].plot()
+            )
+            cv2.waitKey(1)
+        return InGameDetectionVisuals._prediction_cache[cache_id]

royals/decision_makers/mixins/_mob_mixins.py
@@ -12,8 +12,7 @@ class MobsHittingMixin:
     """
     data: BotData
 
-    @staticmethod
-    def mob_count_in_img(img: np.ndarray, mobs: list[BaseMob], **kwargs) -> int:
+    def mob_count_in_img(self, img: np.ndarray, mobs: list[BaseMob], **kwargs) -> int:
         """
         Given an image of arbitrary size, return the mob count of a specific mob found
         within that image.
@@ -21,11 +20,10 @@ class MobsHittingMixin:
         :param mobs: The mobs to look for.
         :return: Total number of mobs detected in the image
         """
-        return sum([mob.get_mob_count(img, **kwargs) for mob in mobs])
+        return sum([mob.get_mob_count(self.data.handle, img, **kwargs) for mob in mobs])
 
-    @staticmethod
     def get_mobs_positions_in_img(
-        img: np.ndarray, mobs: list[BaseMob]
+        self, img: np.ndarray, mobs: list[BaseMob]
     ) -> list[Sequence[int]]:
         """
         Given an image of arbitrary size, return the positions of a specific mob
@@ -35,7 +33,9 @@ class MobsHittingMixin:
         :param mobs: The mobs to look for.
         :return: List of mob positions found in the image.
         """
-        return [pos for mob in mobs for pos in mob.get_onscreen_mobs(img)]
+        return [
+            pos for mob in mobs for pos in mob.get_onscreen_mobs(img, self.data.handle)
+        ]
 
     @staticmethod
     def get_closest_mob_direction(

royals/model/characters/character.py
@@ -114,13 +114,21 @@ class Character(BaseCharacter, ABC):
         if self.detection_model is not None:
             if acceptance_threshold is None:
                 acceptance_threshold = self.DEFAULT_THRESHOLD
-            model_res = self.run_detection_model(handle, image, acceptance_threshold)
-            breakpoint()  # TODO - Fix model_res and debugging mode (merge debugs into base Visual class).
-            if model_res is not None:
+            detections = self.run_detection_model(handle, image, acceptance_threshold)
+            model_res = tuple(
+                [
+                    dct["box"].values() for dct in detections.summary()
+                    if dct["name"] == "Character"
+                ]
+            )
+            if len(model_res) > 1:
+                breakpoint()  # Multiple characters detected, will need to handle this.
+
+            elif len(model_res) == 1:
                 model_res = tuple(map(round, model_res[0]))
-                if DEBUG:
-                    x1, y1, x2, y2 = model_res
-                    cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 3)
+                # if DEBUG:
+                #     x1, y1, x2, y2 = model_res
+                #     cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
         if self._detection_methods is not None:
             detection_res = self._run_detection_methods(image, "single")
@@ -137,12 +145,12 @@ class Character(BaseCharacter, ABC):
         elif self._detection_methods is not None:
             res = detection_res
 
-        if DEBUG:
-            if res is not None:
-                x1, y1, x2, y2 = res
-                cv2.rectangle(image, (x1, y1), (x2, y2), (255, 255, 255), 3)
-            cv2.imshow("_DEBUG_ Character.get_on_screen_position", image)
-            cv2.waitKey(1)
+        # if DEBUG:
+        #     if res is not None:
+        #         x1, y1, x2, y2 = res
+        #         cv2.rectangle(image, (x1, y1), (x2, y2), (255, 255, 255), 3)
+        #     cv2.imshow("_DEBUG_ Character.get_on_screen_position", image)
+        #     cv2.waitKey(1)
         return res
 
     def _preprocess_img(self, image: np.ndarray) -> np.ndarray: